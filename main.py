7dcebb0cd8523bddaf205ac88cfe4c79f309156b
WildKtu <fernandocolaj7@gmail.com>
2026-03-11 08:02:40 -0600

    creacion de la funcion agregar

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ def cargar_datos():
         datos = json.load(archivo)
     return datos
 
-def guardar_datos(datos):
+def guardar_datos():
     with open("datos.json", "w") as archivo:
         json.dump(datos, archivo, indent=4)
+        
+def agregar_servicio():
+    nombre = input("Ingrese el nombre del servicio fotográfico: ")
+    precio = float(input("Ingrese el precio del servicio fotográfico: "))
+    descripcion = input("Ingrese la descripción del servicio fotográfico: ")
+    
+    nuevo_servicio = {
+        "nombre": nombre,
+        "precio": precio,
+        "descripcion": descripcion
+    }
+    
+    guardar_datos.append(nuevo_servicio)
+    print("Servicio fotográfico agregado exitosamente.")   
         
 def separador():
     print("-" * 30)
