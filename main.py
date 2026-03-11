661642734d1a3f82222751e6a1ffa56a78040884
WildKtu <fernandocolaj7@gmail.com>
2026-03-11 07:41:36 -0600

    creacion del menu

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def cargar_datos():
 def guardar_datos(datos):
     with open("datos.json", "w") as archivo:
         json.dump(datos, archivo, indent=4)
+        
+def separador():
+    print("-" * 30)
+
+def menu():
+    print("1. Agregar servicio fotográfico")
+    print("2. Editar servicio fotográfico")
+    print("3. Eliminar servicio fotográfico")
+    print("4. Salir")
